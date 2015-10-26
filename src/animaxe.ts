9f1603591d5812a1a6add182bbb28669fece6426
Tom Larkworthy <larkworthy@google.com>
2015-10-25 22:45:44 -0700

    Added some docs

--- src/animaxe.ts
+++ src/animaxe.ts
@@ -51,9 +51,21 @@ export class Animation {
         var processed = this._attach(upstream);
         return this.after? this.after.attach(processed): processed;
     }
+
     /**
-     * delivers events to this first, then when that animation is finished
-     * the follower consumers events and the values are used as output, until the follower animation completes
+     * send the downstream context of 'this' animation, as the upstream context to supplied animation.
+     * This allows you to chain custom animation.
+     * Ax.move(...).pipe(myAnimation());
+     */
+    pipe(downstream: Animation): Animation {
+        return combine2(this, downstream);
+    }
+
+    /**
+     * delivers upstream events to 'this' first, then when 'this' animation is finished
+     * the upstream is switched to the the follower animation.
+     * This allows you to sequence animations temporally.
+     * frame1Animation().then(frame2Animation).then(frame3Animation)
      */
     then(follower: Animation): Animation {
         var self = this;
@@ -121,31 +133,45 @@ export class Animation {
             }).subscribeOn(Rx.Scheduler.immediate); //todo remove subscribeOns
         });
     }
-
-    pipe(after: Animation): Animation {
-        return combine2(this, after);
+    /**
+     * Creates an animation that replays the inner animation each time the inner animation completes.
+     * The resultant animation is always runs forever while upstream is live. Only a single inner animation
+     * plays at a time (unlike emit())
+     */
+    loop(inner: Animation): Animation {
+        return this.pipe(loop(inner));
+    }
+    /**
+     * Creates an animation that sequences the inner animation every time frame
+     * The resultant animation is always runs forever while upstream is live. Multiple inner animations
+     * can be playing at the same time (unlike loop)
+     */
+    emit(inner: Animation): Animation {
+        return this.pipe(emit(inner));
     }
 
-    translate(position: PointArg): Animation {
-        return this.pipe(translate(position));
+    /**
+     * Plays all the inner animations at the same time. Parallel completes when all inner animations are over.
+     * The canvas states are restored each time, so styling and transforms of different animations do not
+     * affect each other (although obsviously the pixel buffer is affected by each animation)
+     */
+    parallel(inner_animations: Rx.Observable<Animation> | Animation[]): Animation {
+        return this.pipe(parallel(inner_animations));
     }
+
+    /**
+     * Sequences n copies of the inner animation. Clone completes when all inner animations are over.
+     */
+    clone(n: number, inner: Animation): Animation {
+        return this.pipe(clone(n, inner));
+    }
+    /**
+     * translates the drawing context by velocity * tick.clock
+     */
     velocity(vector: PointArg): Animation {
         return this.pipe(velocity(vector));
     }
 
-    loop(inner: Animation): Animation {
-        return this.pipe(loop(inner));
-    }
-
-    emit(inner: Animation): Animation {
-        return this.pipe(emit(inner));
-    }
-    clone(n: number, inner: Animation): Animation {
-        return this.pipe(clone(n, inner));
-    }
-    parallel(inner: Rx.Observable<Animation> | Animation[]): Animation {
-        return this.pipe(parallel(inner));
-    }
     tween_linear(
         from: PointArg,
         to:   PointArg,
@@ -153,35 +179,73 @@ export class Animation {
         return this.pipe(tween_linear(from, to, time));
     }
 
+    /**
+     * Creates an animation that is at most n frames from 'this'.
+     */
     take(frames: number): Animation {
         return this.pipe(take(frames));
     }
+
+    /**
+     * helper method for implementing simple animations (that don't fork the animation tree).
+     * You just have to supply a function that does something with the draw tick.
+     */
     draw(drawFactory: () => ((tick: DrawTick) => void)): Animation {
         return this.pipe(draw(drawFactory));
     }
 
     // Canvas API
+    /**
+     * Dynamic chainable wrapper for translate in the canvas API.
+     */
+    translate(position: PointArg): Animation {
+        return this.pipe(translate(position));
+    }
+    /**
+     * Dynamic chainable wrapper for strokeStyle in the canvas API.
+     */
     strokeStyle(color: ColorArg): Animation {
         return this.pipe(strokeStyle(color));
     }
+    /**
+     * Dynamic chainable wrapper for fillStyle in the canvas API.
+     */
     fillStyle(color: ColorArg): Animation {
         return this.pipe(fillStyle(color));
     }
+    /**
+     * Dynamic chainable wrapper for fillRect in the canvas API.
+     */
     fillRect(xy: PointArg, width_height: PointArg): Animation {
         return this.pipe(fillRect(xy, width_height));
     }
+    /**
+     * Encloses the inner animation with a beginpath() and endpath() from the canvas API.
+     */
     withinPath(inner: Animation): Animation {
         return this.pipe(withinPath(inner));
     }
+    /**
+     * Dynamic chainable wrapper for moveTo in the canvas API. Use with withinPath.
+     */
     moveTo(xy: PointArg): Animation {
         return this.pipe(moveTo(xy));
     }
+    /**
+     * Dynamic chainable wrapper for lineTo in the canvas API. Use with withinPath.
+     */
     lineTo(xy: PointArg): Animation {
         return this.pipe(lineTo(xy));
     }
+    /**
+     * Dynamic chainable wrapper for stroke in the canvas API.
+     */
     stroke(): Animation {
         return this.pipe(stroke());
     }
+    /**
+     * * Dynamic chainable wrapper for globalCompositeOperation in the canvas API.
+     */
     globalCompositeOperation(operation: string): Animation {
         return this.pipe(globalCompositeOperation(operation));
     }
